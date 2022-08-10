77a11f6ef29b9ba95e5430998eefa00a8b49670b
faraja17 <farajakthompson@gmail.com>
2022-08-10 19:42:06 -0400

    added input validation

--- wordle.py
+++ wordle.py
@@ -10,28 +10,31 @@
     # count of tries +1
 
 def wordle():
-    print("Welcome to Wordle!\n Guess the 5-letter secret word.\n") 
+    print("\nWelcome to Wordle!\nGuess the 5-letter secret word.\n") 
   
     computer_word = 'apple'
     guess_count = 1
 
     while guess_count < 7:
-        results = ""
-        player_word = input("What is your guess?")
-        for i in range(5):
-            if player_word[i] == computer_word[i]:
-                results += "G"
-            elif player_word[i] in computer_word:
-                results += "Y"
-            else:
-                results += "B"
-            # print(str(guess_count) + "/6 guesses. " + str(results))
-        print(f"{guess_count} /6 guesses. Results: {results}")    
-            # if win; else
-        if results == "GGGGG":
-            print("Congratulations, you won!")
+      results = ""
+      player_word = input("What is your guess?\n")
+      if len(player_word) != 5:
+        print("Guess must have exactly 5 letters. Please try again.")
+        continue
+      for i in range(5):
+        if player_word[i] == computer_word[i]:
+            results += "G"
+        elif player_word[i] in computer_word:
+            results += "Y"
         else:
-            guess_count += 1
+            results += "B"
+        # print(str(guess_count) + "/6 guesses. " + str(results))
+      print(f"{guess_count} /6 guesses. Results: {results}")    
+        # if win; else
+      if results == "GGGGG":
+        print("Congratulations, you won!")
+      else:
+        guess_count += 1
     print("You have run out of chances!")  
 
 wordle()
@@ -41,3 +44,4 @@ wordle()
     # fix guess count to start at 1 rather than at 0
     # input validation
     # play again option
+    # refactor with an object-oriented approach

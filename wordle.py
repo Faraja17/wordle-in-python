89535fd2eed495750cb45805dbff6f08af674d3f
faraja17 <farajakthompson@gmail.com>
2022-08-10 21:18:29 -0400

    changed wording around, added greeting()

--- wordle.py
+++ wordle.py
@@ -9,35 +9,37 @@
     # else return the results list wit all Bs
     # count of tries +1
 
-def wordle():
-    print("\nWelcome to Wordle!\nGuess the 5-letter secret word.\n") 
-  
-    computer_word = 'apple'
-    guess_count = 1
+def greeting():
+  print("\nWelcome to Wordle!\nGuess the 5-letter secret word.\n") 
 
-    while guess_count < 7:
-      results = ""
-      player_word = input("What is your guess?\n")
-      if len(player_word) != 5:
-        print("Guess must have exactly 5 letters. Please try again.")
-        continue
-      for i in range(5):
-        if player_word[i] == computer_word[i]:
-            results += "G"
-        elif player_word[i] in computer_word:
-            results += "Y"
-        else:
-            results += "B"
-        # print(str(guess_count) + "/6 guesses. " + str(results))
-      print(f"{guess_count} /6 guesses. Results: {results}")    
-        # if win; else
-      if results == "GGGGG":
-        print("Congratulations, you won!")
-        playAgain()
+
+def wordle():
+  computer_word = 'apple'
+  guess_count = 1
+
+  while guess_count < 7:
+    results = ""
+    player_word = input(f"Guess {guess_count}/6. What is your guess?\n")
+    if len(player_word) != 5:
+      print("Guess must have exactly 5 letters. Please try again.")
+      continue
+    for i in range(5):
+      if player_word[i] == computer_word[i]:
+          results += "G"
+      elif player_word[i] in computer_word:
+          results += "Y"
       else:
-        guess_count += 1
-    print(f"You have run out of chances! Secret word: {computer_word}")
-    playAgain()  
+          results += "B"
+      # print(str(guess_count) + "/6 guesses. " + str(results))
+    print(f"Results: {results}")    
+      # if win; else
+    if results == "GGGGG":
+      print("Congratulations, you won!")
+      playAgain()
+    else:
+      guess_count += 1
+  print(f"You have run out of chances! Secret word: {computer_word}")
+  playAgain()  
 
 def playAgain():
   choice = input("\nPlay again? y or n\n")
@@ -47,6 +49,7 @@ def playAgain():
     print("\nThank you for playing!")
     exit()
 
+greeting()
 wordle()
 
 # paired on this code in replit with Jing!
